9428e6ffd42b78cccfbce47598c580b8edbc946e
CaritoDasilva <caritodasilva@gmail.com>
2021-02-07 17:25:02 -0300

    se agrega headers a la llamada

--- pages/api/pharmacy_service.ts
+++ pages/api/pharmacy_service.ts
@@ -1,11 +1,11 @@
 import axios from 'axios';
-
+import { headers } from '../../next.config';
 
 export default class PharmacyService {
 
     async getCommunesAndPharmacys() {
         try {
-            let data = await axios.get('http://18.221.149.135/api/communes/7')
+            let data = await axios.get('http://18.221.149.135/api/communes/7', { headers: headers() })
             if (data) {
                 return data.data
             }
