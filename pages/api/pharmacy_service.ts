f124f226c83b1b5173bc1aefd6cd144eb407c285
CaritoDasilva <caritodasilva@gmail.com>
2021-02-07 18:10:12 -0300

    testing dns https

--- pages/api/pharmacy_service.ts
+++ pages/api/pharmacy_service.ts
@@ -4,7 +4,7 @@ export default class PharmacyService {
 
     async getCommunesAndPharmacys() {
         try {
-            let data = await axios.get('http://18.221.149.135/api/communes/7')
+            let data = await axios.get('https://ec2-18-221-149-135.us-east-2.compute.amazonaws.com/api/communes/7')
             if (data) {
                 console.log("🚀 ~ file: pharmacy_service.ts ~ line 10 ~ PharmacyService ~ getCommunesAndPharmacys ~ data", data)
                 return data.data
