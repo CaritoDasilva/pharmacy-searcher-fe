498a03ab6768d99f722b4ea8dd69db5b589b3678
CaritoDasilva <caritodasilva@gmail.com>
2021-02-07 19:27:34 -0300

    se cambia url a deploy heroku

--- pages/api/pharmacy_service.ts
+++ pages/api/pharmacy_service.ts
@@ -4,7 +4,7 @@ export default class PharmacyService {
 
     async getCommunesAndPharmacys() {
         try {
-            let data = await axios.get('https://ec2-18-221-149-135.us-east-2.compute.amazonaws.com/api/communes/7')
+            let data = await axios.get('https://pharmacy-searcher-be.herokuapp.com/api/communes/7')
             if (data) {
                 console.log("🚀 ~ file: pharmacy_service.ts ~ line 10 ~ PharmacyService ~ getCommunesAndPharmacys ~ data", data)
                 return data.data
