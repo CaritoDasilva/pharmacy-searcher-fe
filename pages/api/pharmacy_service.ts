53d80cb4bcdc26063cefe5fc56857513044d1c8b
CaritoDasilva <caritodasilva@gmail.com>
2021-02-07 17:44:12 -0300

    test headers en next config

--- pages/api/pharmacy_service.ts
+++ pages/api/pharmacy_service.ts
@@ -5,8 +5,9 @@ export default class PharmacyService {
 
     async getCommunesAndPharmacys() {
         try {
-            let data = await axios.get('http://18.221.149.135/api/communes/7', { headers: headers })
+            let data = await axios.get('http://18.221.149.135/api/communes/7')
             if (data) {
+                console.log("🚀 ~ file: pharmacy_service.ts ~ line 10 ~ PharmacyService ~ getCommunesAndPharmacys ~ data", data)
                 return data.data
             }
         }
